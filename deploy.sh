08c73912ce7a643e5c34d537c23204b7b6ebc6be
Daniel Mauno Pettersson <daniel@dooer.com>
2018-10-12 15:46:08 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ docker build -t vattenfallds .
 docker tag vattenfallds eu.gcr.io/vattenfall-design-system/vattenfallds
 docker push eu.gcr.io/vattenfall-design-system/vattenfallds
 
-# gcloud compute instances update-container vf-docker-instance --container-image eu.gcr.io/vattenfall-design-system/vattenfallds
-gcloud compute instances reset vf-docker-instance
+gcloud compute instances update-container vf-docker-instance --container-image eu.gcr.io/vattenfall-design-system/vattenfallds --zone europe-north1-a
+gcloud compute instances reset vf-docker-instance --zone europe-north1-a
