47f403300e80e79a57f91f44f6482eb8e96b0642
Daniel Mauno Pettersson <daniel@mauno.io>
2018-12-11 13:42:54 +0100

    fix: Update font folder path

--- catalog/contribute.md
+++ catalog/contribute.md
@@ -1,3 +1,1 @@
-# How to Contribute
-
-asdsads
+The contribution guidelines are available on the main Horizon website, check out https://clients.alchemy.se/vattenfall/
---
+++ js/components/hero.js
@@ -0,0 +1,8 @@
+var rellax = new Rellax('.vf-two-column-boxes', {
+  speed: -2,
+  center: false,
+  wrapper: null,
+  round: true,
+  vertical: true,
+  horizontal: false
+});
