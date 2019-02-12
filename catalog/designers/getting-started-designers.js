ff6ab58af915a1df515313dfa894410f8e0cb2ed
Daniel Mauno Pettersson <elena.karlsen@yahoo.com>
2019-02-12 17:04:38 +0100

    feat: example page added

--- catalog/designers/getting-started-designers.js
+++ catalog/designers/getting-started-designers.js
@@ -15,31 +15,31 @@ const Designers = () => (
         In order to get started with Vattenfall Digital Design System, make sure you have been provided with both apps: Abstract and Sketch. Abstract is a version control tool, which keeps all of your Sketch working files in one place. It also prevents creating conflicting copies and allows contributors to request reviews and inspect the files without opening Sketch.
         </p>
 
-        <div class="vf-row">
-          <div class="vf-col">
-        <a class="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/how-it-works/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/how-it-works/">Get Abstract</a>
+        <div className="vf-row">
+          <div className="vf-col">
+        <a className="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/how-it-works/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/how-it-works/">Get Abstract</a>
           </div>
-          <div class="vf-col">
+          <div className="vf-col">
         </div>
       </div> 
 
-        <div class="vf-row">
-          <div class="vf-col">
-        <a class="vf-link-with-arrow vf-link-with-arrow--small" href="https://sketchapp.com/docs/the-interface/" target="_blank" rel="noopener" data-mce-href="https://sketchapp.com/docs/the-interface/">Sketch Introduction</a>
+        <div className="vf-row">
+          <div className="vf-col">
+        <a className="vf-link-with-arrow vf-link-with-arrow--small" href="https://sketchapp.com/docs/the-interface/" target="_blank" rel="noopener" data-mce-href="https://sketchapp.com/docs/the-interface/">Sketch Introduction</a>
            </div>
-          <div class="vf-col">
+          <div className="vf-col">
         </div>
       </div>
 
-      <div class="vf-row">
-        <div class="vf-col">
-      <a class="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/blog/making-sketch-collaboration-better-with-abstract/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/blog/making-sketch-collaboration-better-with-abstract/">Sketch + Abstract collaboration</a>
+      <div className="vf-row">
+        <div className="vf-col">
+      <a className="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/blog/making-sketch-collaboration-better-with-abstract/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/blog/making-sketch-collaboration-better-with-abstract/">Sketch + Abstract collaboration</a>
         </div>
-        <div class="vf-col">
+        <div className="vf-col">
       </div>
     </div> 
 
-    <iframe style={{marginTop: '40px'}} src="https://player.vimeo.com/video/305508035?color=ffda00&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe style={{marginTop: '40px'}} src="https://player.vimeo.com/video/305508035?color=ffda00&title=0&byline=0&portrait=0" width="640" height="360" frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe>
 <p><a href="https://vimeo.com/305508035">Horizon: Abstract &amp; Sketch Walkthrough</a> from <a href="https://vimeo.com/alchemyse">Alchemy</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
 
       <h4 style={{marginTop: '40px'}}>Vattenfall Design Kit</h4>
@@ -47,22 +47,22 @@ const Designers = () => (
         The Vattenfall Design Kit (VDK) is a living library that contains Vattenfall's digital visual assets for everyday purpose: modules, components, icons, color palettes, grids, typography, etc. The purpose of VDK is to ensure design and development consistency across entire Vattenfall ecosystem, improve quality & efficiency in production, bring digital products & services to next level of coherency. The VDK can be accessed via Abstract in Sketch format and connected to your working design documents as a Sketch Library source.
         </p>
 
-    <iframe style={{marginTop: '40px'}}src="https://player.vimeo.com/video/305756714?color=ffda00&title=0&byline=0&portrait=0" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe style={{marginTop: '40px'}}src="https://player.vimeo.com/video/305756714?color=ffda00&title=0&byline=0&portrait=0" width="640" height="400" frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen></iframe>
 <p><a href="https://vimeo.com/305756714">Vattenfall Design Kit</a> from <a href="https://vimeo.com/alchemyse">Alchemy</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
        
-        <div class="vf-row">
-          <div class="vf-col">
-        <a class="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/how-it-works/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/how-it-works/">Vattenfall Design Kit on Abstract</a>
+        <div className="vf-row">
+          <div className="vf-col">
+        <a className="vf-link-with-arrow vf-link-with-arrow--small" href="https://www.goabstract.com/how-it-works/" target="_blank" rel="noopener" data-mce-href="https://www.goabstract.com/how-it-works/">Vattenfall Design Kit on Abstract</a>
           </div>
-        <div class="vf-col">
+        <div className="vf-col">
       </div>
     </div> 
 
-    <div class="vf-row">
-          <div class="vf-col">
-        <a class="vf-link-with-arrow vf-link-with-arrow--small" href="https://sketchapp.com/docs/the-interface/" target="_blank" rel="noopener" data-mce-href="https://sketchapp.com/docs/the-interface/" data-mce-selected="1">Vattenfall Design Kit on Github</a>
+    <div className="vf-row">
+          <div className="vf-col">
+        <a className="vf-link-with-arrow vf-link-with-arrow--small" href="https://sketchapp.com/docs/the-interface/" target="_blank" rel="noopener" data-mce-href="https://sketchapp.com/docs/the-interface/" data-mce-selected="1">Vattenfall Design Kit on Github</a>
         </div>
-        <div class="vf-col">
+        <div className="vf-col">
       </div>
     </div> 
   
@@ -73,16 +73,15 @@ const Designers = () => (
         </p>
 
       <p style={{ fontSize: '18px' }}>
-      The library of Design Kit contains more that 500 ready-to-use design snippets: from simply buttons to more complex modules. Make sure to study the component library as well and use components with correct naming.
-      We use a simplified version of the Atomic Design methodology, which we have adapted to suit our design process and our team culture. The main difference with Atomic Design is that all modules are either an atom or a molecule, categorised by purpose. Having organisms used to cause confusion in the team, so we removed them. Our approach to defining modules and naming them is function directed, rather than presentational. It is based on evolving a shared design language collaboratively and empowering all designers and engineers on the team to contribute to the system.
+      The library of Design Kit contains more that 500 ready-to-use design snippets: from simple buttons to more complex modules. Make sure to study the component library as well and use components with correct naming.
+      We use a simplified version of the Atomic Design methodology, which we have adapted to suit our design process and our team culture. The main difference with Atomic Design is that all modules are either an atom or a molecule, categorised by purpose. Our approach to defining modules and naming them is function-directed, rather than presentational. It is based on evolving a shared design language collaboratively and empowering all designers and engineers on the team to contribute to the system.
       </p>
 
-     <div>
-       <img class="is-content" src="https://clients.alchemy.se/vattenfall/wp-content/uploads/2018/12/structure-1.png" width="50%" alt="structure" caption="" data-width="original" data-scaling="no"></img></div>
-     <div>
-       <br></br>
-       <img class="is-content" src="https://clients.alchemy.se/vattenfall/wp-content/uploads/2018/12/500-2.png" width="50%" alt="500" caption="" data-width="original" data-scaling="no"></img></div>
-    </div> 
-  </div> 
+   
+       <img className="is-content" src="https://clients.alchemy.se/vattenfall/wp-content/uploads/2018/12/structure-1.png" width="70%" alt="structure" caption="" data-width="original" data-scaling="no"></img></div>
+     
+       <br></br> 
+        </div> 
+  
 )
 export default Designers;
