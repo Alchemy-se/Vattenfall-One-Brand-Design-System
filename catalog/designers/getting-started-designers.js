9fc6dbf1db3edb7da35e704d73a9c8a017c9f569
Daniel Mauno Pettersson <elena.karlsen@yahoo.com>
2019-02-12 14:58:38 +0100

    feat: pictures added, top spacetop marging added to the videos

--- catalog/designers/getting-started-designers.js
+++ catalog/designers/getting-started-designers.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Designers = () => (
-  <div style= {{ padding: '40px'}}>
+  <div style= {{ padding: '40px', width: '70%'}}>
     <link href="/dist/css/reboot.css" rel="stylesheet" type="text/css" />
     <link href="/dist/css/main.css" rel="stylesheet" type="text/css" />
     <link href="/dist/css/documentation.css" rel="stylesheet" type="text/css" />
@@ -39,7 +39,7 @@ const Designers = () => (
       </div>
     </div> 
 
-    <iframe src="https://player.vimeo.com/video/305508035?color=ffda00&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe style={{marginTop: '40px'}} src="https://player.vimeo.com/video/305508035?color=ffda00&title=0&byline=0&portrait=0" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 <p><a href="https://vimeo.com/305508035">Horizon: Abstract &amp; Sketch Walkthrough</a> from <a href="https://vimeo.com/alchemyse">Alchemy</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
 
       <h4 style={{marginTop: '40px'}}>Vattenfall Design Kit</h4>
@@ -47,7 +47,7 @@ const Designers = () => (
         The Vattenfall Design Kit (VDK) is a living library that contains Vattenfall's digital visual assets for everyday purpose: modules, components, icons, color palettes, grids, typography, etc. The purpose of VDK is to ensure design and development consistency across entire Vattenfall ecosystem, improve quality & efficiency in production, bring digital products & services to next level of coherency. The VDK can be accessed via Abstract in Sketch format and connected to your working design documents as a Sketch Library source.
         </p>
 
-        <iframe src="https://player.vimeo.com/video/305756714?color=ffda00&title=0&byline=0&portrait=0" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <iframe style={{marginTop: '40px'}}src="https://player.vimeo.com/video/305756714?color=ffda00&title=0&byline=0&portrait=0" width="640" height="400" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 <p><a href="https://vimeo.com/305756714">Vattenfall Design Kit</a> from <a href="https://vimeo.com/alchemyse">Alchemy</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
        
         <div class="vf-row">
@@ -77,6 +77,11 @@ const Designers = () => (
       We use a simplified version of the Atomic Design methodology, which we have adapted to suit our design process and our team culture. The main difference with Atomic Design is that all modules are either an atom or a molecule, categorised by purpose. Having organisms used to cause confusion in the team, so we removed them. Our approach to defining modules and naming them is function directed, rather than presentational. It is based on evolving a shared design language collaboratively and empowering all designers and engineers on the team to contribute to the system.
       </p>
 
+     <div>
+       <img class="is-content" src="https://clients.alchemy.se/vattenfall/wp-content/uploads/2018/12/structure-1.png" width="50%" alt="structure" caption="" data-width="original" data-scaling="no"></img></div>
+     <div>
+       <br></br>
+       <img class="is-content" src="https://clients.alchemy.se/vattenfall/wp-content/uploads/2018/12/500-2.png" width="50%" alt="500" caption="" data-width="original" data-scaling="no"></img></div>
     </div> 
   </div> 
 )
