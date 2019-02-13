c98b32fe28079fb7e589aeeb95df8a3f7f97ccb0
Daniel Mauno Pettersson <elena.karlsen@yahoo.com>
2019-02-13 10:12:59 +0100

    fix: welcome page paragraphs width and margins fixed

--- catalog/WELCOME.js
+++ catalog/WELCOME.js
@@ -1,15 +1,12 @@
 import React from "react";
 
 const Welcome = () => (
-  <div style={{ margin: '0 30px 0 40px' }}>
+  <div style={{ margin: '0 20px 0 40px' }}>
     <div style={{
-      height: '70vh',
+      height: '50vh',
       maxWidth: '750px',
       background: 'transparent'
     }}>
-      <p style={{ fontSize: '18px' }} className="vf-pt-md">
-        Welcome to the technical documentation for Vattenfall Design System.
-      </p>
 
       <p style={{ fontSize: '18px' }} className="">
       Vattenfall Design System unites products & services around a common visual language. It aims to reduce current Design Debt, accelerates the design process, and builds bridges between teams working in concert to bring unified products to life within set brand. Vattenfall Design System is created with aim to help the teams within Vattenfall to improve product quality, amplify production efficiency and enhance communication.
@@ -22,7 +19,7 @@ const Welcome = () => (
     </div> 
 
     <img src="/img/docs/illustration.png" style={{ position: 'fixed', right: 0, top: 0, zIndex: -1, height: '100%', objectFit: 'contain', maxWidth: '30%' }} />
-
+    <div style={{width: '70%'}}>
     <h4>Designers</h4>
     <p>
       Get the latest Design System Sketch library through abstract. Read the <a href="/designers">Getting Started for Designers</a> page.
@@ -46,6 +43,7 @@ const Welcome = () => (
     <p>
       Are you rather looking for a general styleguide with information about how to use Vattenfalls logotype, colors and such? Then you will find everything you need over at https://clients.alchemy.se/vattenfall/.
     </p>
+    </div>
   </div>
 )
 
