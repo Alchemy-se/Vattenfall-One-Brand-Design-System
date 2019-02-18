24fadc1a2e2d949aceb1347788fc0b3e273c0a14
Daniel Mauno Pettersson <elena.karlsen@yahoo.com>
2019-02-18 12:32:30 +0100

    feat: links added to the examples page

--- catalog/EXAMPLES.js
+++ catalog/EXAMPLES.js
@@ -16,10 +16,9 @@ const Examples = () => (
             <h4 className="imageHeader">Homepage</h4>
               <div className="vf-col">
               <a href="./img/use-cases/Web/Homepage-Desktop.jpg">
-                <figure>
                    <img className="examplesImage" src="./img/use-cases/Web/Homepage-Desktop.jpg"></img>
-                    <figcaption style={{ color: '#888888', marginTop: '20px'}}>Desktop</figcaption>
-                </figure>
+                  <p style={{marginTop: "20px"}}><a style={{color: "#888888"}} href="/start.html" target="_blank" rel="noopener">See the coded page</a></p>
+                  <p><a style={{color: "#888888"}} href="https://share.goabstract.com/6f85c042-9b71-4bcd-96a5-9c27a2b2a581" target="_blank" rel="noopener">Go to Abstract</a></p>
               </a>
               </div>
           </div>
@@ -27,10 +26,9 @@ const Examples = () => (
             <h4 className="imageHeader">Article</h4>
               <div className="vf-col">
               <a href="./img/use-cases/Web/Article-Desktop.jpg">
-                <figure>
                   <img className="examplesImage" src="./img/use-cases/Web/Article-Desktop.jpg"></img>
-                  <p style={{ color: '#888888', marginTop: '20px'}}>Desktop</p>  
-                </figure>
+                  <p style={{marginTop: "20px"}}><a style={{color: "#888888"}} href="./article.html" target="_blank" rel="noopener">See the coded page</a></p>
+                  <p><a style={{color: "#888888"}} href="https://share.goabstract.com/cb9164a3-976c-474d-a351-72906a7f4c13" target="_blank" rel="noopener">Go to Abstract</a></p>
               </a>
               </div> 
           </div>
@@ -40,10 +38,9 @@ const Examples = () => (
             <h4 className="imageHeader">Secondary page</h4>
               <div className="vf-col">
               <a href="./img/use-cases/Web/Investors-Desktop.jpg">
-              <figure>
-                <img className="examplesImage" src="./img/use-cases/Web/Investors-Desktop.jpg"></img>
-                <figcaption style={{ color: '#888888', marginTop: '20px'}}>Desktop</figcaption>  
-              </figure>
+                  <img className="examplesImage" src="./img/use-cases/Web/Investors-Desktop.jpg"></img>
+                  <p style={{marginTop: "20px"}}><a style={{color: "#888888"}} href="./investors.html" target="_blank" rel="noopener">See the coded page</a></p>
+                  <p><a style={{color: "#888888"}} href="https://share.goabstract.com/f4fa75b4-6b07-46ca-a84b-2a0fe44d8e50" target="_blank" rel="noopener">Go to Abstract</a></p>
               </a>
               </div>
             </div>
