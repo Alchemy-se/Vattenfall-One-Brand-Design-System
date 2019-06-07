1adba651c84bd9b60b83f69632f7eb27736b98f1
Chris Hall <chris.hall@alchemy.se>
2019-06-07 16:25:48 +0200

    WIP: Input fields in various states for review

--- js/components/input-field.js
+++ js/components/input-field.js
@@ -2,6 +2,18 @@
 
 	$(".vf-input").each(function( index ) {
 
+		if($(this).attr('type') == "number"){
+			$('<div class="vf-input--number-decrement" onclick="if($.isNumeric(parentNode.querySelector(\'.vf-input\').value)){parentNode.querySelector(\'.vf-input\').stepDown()}else{parentNode.querySelector(\'.vf-input\').value = 0}"></div>').insertAfter(this);
+			$('<div class="vf-input--number-increment" onclick="if($.isNumeric(parentNode.querySelector(\'.vf-input\').value)){parentNode.querySelector(\'.vf-input\').stepUp()}else{parentNode.querySelector(\'.vf-input\').value = 0}"></div>').insertAfter(this);
+			
+			$('.vf-input--number-increment, .vf-input--number-decrement').on('click',function(){
+				//$(this).siblings('.vf-input').val()
+				$(this).siblings('.vf-input-tooltip-placeholder').css("opacity", "1");
+
+			});
+
+		}
+
 		if ($(this).data("tooltip")){
 
 			let tooltip = $(this).data("tooltip");
@@ -9,6 +21,28 @@
 			$('<div class="vf-input-tooltip-message">' + tooltip + '</div>').insertAfter(this);
 			$('<div class="vf-input-tooltip-icon"></div>').insertAfter(this);
 
+		}
+
+		if ($(this).attr("placeholder")){
+
+			//Allow the input to grow according to placeholder size. CSS min-width 264px;
+			$(this).attr('size', $(this).attr('placeholder').length + 3);
+
+			//Insert placeholder text above input
+			let placeholder = $(this).attr('placeholder');
+			$('<div class="vf-input-tooltip-placeholder">' + placeholder + '</div>').insertAfter(this);
+
+			//Show / hide
+			$(this).on('keyup',function(){
+			    if($(this).val().length > 0){
+					$(this).siblings('.vf-input-tooltip-placeholder').css("opacity", "1");
+				}
+				else if($(this).val().length === 0){
+					$(this).siblings('.vf-input-tooltip-placeholder').css("opacity", "0");
+				}
+		    });
+
+		    
 		}
 
 	});
