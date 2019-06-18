ae4a5d40ac635dfc91023b1cc705032a0b7aafeb
Chris Hall <chris.hall@alchemy.se>
2019-06-18 15:47:39 +0200

    fix: Change Placeholder to Label

--- js/components/input-field.js
+++ js/components/input-field.js
@@ -1,61 +1,88 @@
 (function($){
 
-	$(".vf-input").each(function( index ) {
+console.log("werwe"); //Called twice. Jquery included twice?
 
-		if($(this).attr('type') == "number"){
-			$('<div class="vf-input--number-decrement" onclick="if($.isNumeric(parentNode.querySelector(\'.vf-input\').value)){parentNode.querySelector(\'.vf-input\').stepDown()}else{parentNode.querySelector(\'.vf-input\').value = 0}"></div>').insertAfter(this);
-			$('<div class="vf-input--number-increment" onclick="if($.isNumeric(parentNode.querySelector(\'.vf-input\').value)){parentNode.querySelector(\'.vf-input\').stepUp()}else{parentNode.querySelector(\'.vf-input\').value = 0}"></div>').insertAfter(this);
+	$('.vf-input').each(function( index ) {
+
+		$(this).on('keyup',function(){
+
+			if($(this).val().length === 0){
+
+				if($(this).hasClass('vf-input--data')){
+					$(this).removeClass('vf-input--data');
+				}
+			}
+			else{
+				//Input has been entered. Prevent label from returning to original position.
+				if(!$(this).hasClass('vf-input--data')){
+					$(this).addClass('vf-input--data');
+				}
+			}
+		});
+
+		if($(this).attr('type') == 'number'){ //use class instread...?
 			
-			$('.vf-input--number-increment, .vf-input--number-decrement').on('click keyup',function(){
-				//$(this).siblings('.vf-input').val()
-				$(this).siblings('.vf-input-tooltip-placeholder').css("opacity", "1");
+
+/*
+			//minus not being inputted. Restricted by "number"?
+			//Numbers only
+			this.oninput = function(value){
+
+				this.value = this.value.replace((/[^0-9.]/g), ''); //Does not allow minus
+				this.value = this.value.replace(/(\..*)\./g, '$1');
+
+			};
+*/
+
+
+			if (!$(this).siblings().hasClass('vf-input--number-decrement')) {
+				$('<div class="vf-input--number-decrement"></div>').insertAfter(this);
+			}
+
+			if (!$(this).siblings().hasClass('vf-input--number-increment')) {
+				$('<div class="vf-input--number-increment"></div>').insertAfter(this);
+			}
+
+			$('.vf-input--number-increment, .vf-input--number-decrement').unbind().on('click keyup',function(){ //unbind() Prevents double call
+
+				if(!$(this).closest('.vf-input-container').find('.vf-input').hasClass("vf-input--data")){
+
+					//Move label
+					$(this).closest('.vf-input-container').find('.vf-input').addClass("vf-input--data");
+
+				}
+
+				if($(this).hasClass("vf-input--number-increment")){
+
+					if($.isNumeric(this.parentNode.querySelector('.vf-input').value)){
+						this.parentNode.querySelector('.vf-input').stepUp();
+					}
+					else{
+						this.parentNode.querySelector('.vf-input').value = 1;
+					}
+
+				}
+
+				else if($(this).hasClass("vf-input--number-decrement")){
+
+					if($.isNumeric(this.parentNode.querySelector('.vf-input').value)){
+						this.parentNode.querySelector('.vf-input').stepDown();
+					}
+					else{
+						this.parentNode.querySelector('.vf-input').value = -1;
+					}
+				}
 
 			});
 		}
 
-		if ($(this).data("tooltip")){
+		if ($(this).data('tooltip')){
 
 			let tooltip = $(this).data("tooltip");
 
 			$('<div class="vf-input-tooltip-message">' + tooltip + '</div>').insertAfter(this);
 			$('<div class="vf-input-tooltip-icon"></div>').insertAfter(this);
 
-		}
-
-		if ($(this).attr("placeholder")){
-
-			//Allow the input to grow according to placeholder size. CSS min-width 264px;
-			//$(this).attr('size', $(this).attr('placeholder').length + 3);
-			$(this).attr('size', $(this).attr('placeholder').length);
-
-
-			//If label does not exist, create one from the placeholder.
-			if (!$(this).siblings('label').length){
-
-				//Insert placeholder text above input
-				let placeholder = $(this).attr('placeholder');
-
-				//If input has ID, add "for".
-				let forAttr = "";
-				if ($(this).attr("id")){
-					forAttr = 'for="' + $(this).attr("id") + '"';
-				}
-
-				$('<label ' + forAttr + '>' + placeholder + '</label>').insertAfter(this);
-
-			}
-
-			//Show / hide
-			$(this).on('keyup',function(){
-			    if($(this).val().length > 0){
-					$(this).siblings('label').css("opacity", "1");
-				}
-				else if($(this).val().length === 0){
-					$(this).siblings('label').css("opacity", "0");
-				}
-		    });
-
-		    
 		}
 
 	});
