8b3bfa8c63946e9f7c546bec051cafcdc79a2188
Chris Hall <chris.hall@alchemy.se>
2019-07-09 18:50:46 +0200

    WIP: AJAX

--- catalog/components/file-upload-module.md
+++ catalog/components/file-upload-module.md
@@ -1,39 +1,23 @@
 
-
-
-Add note about accept: <input type="file"
-  accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
-https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#attr-accept
-
-Validation, etc...
-
-Have a transition if files upload quickly?
-
-
 ```html
 showSource: true
 plain: false
 ---
 
-	<form class="vf-file-upload-module" method="post" action="/" enctype="multipart/form-data">
+	<form class="vf-file-upload-module" method="post" action="http://localhost:3005/uploads/upload.php" enctype="multipart/form-data">
 
-		<div class="vf-file-upload-module__icon">
+		<div class="vf-file-upload-module__icon vf-file-upload-module__icon--doc">
 		</div>
 
 		<div class="vf-file-upload-module__input-container">
 
-			<div class="vf-file-upload-module__input--heading vf-file-upload-module__input--heading-uploading">Uploading&hellip;</div>
-			<div class="vf-file-upload-module__input--heading vf-file-upload-module__input--heading-drag">Drag & drop your file(s) here</div>
-			<div class="vf-file-upload-module__input--heading vf-file-upload-module__input--heading-files"></div>
+			<div class="vf-file-upload-module__input--heading" data-text-uploading="Uploading&hellip;" data-text-drag="Drag & drop your file(s) here" data-text-multiple-uploaded="files uploaded" data-text-single-uploaded="uploaded"></div>
 
-			<label class="vf-file-upload-module__input--label-no-drag">
-				<div class="vf-file-upload-module__input--heading vf-file-upload-module__input--heading-select">Select file</div>
-				<input class="vf-file-upload-module__input-file" type="file" name="files[]" data-multiple-caption="{count} files selected" multiple />
+			<label class="vf-file-upload-module__input--label-no-drag">Select file(s)
+				<input type="file" class="vf-file-upload-module__input-file" name="files[]" data-text-number-of-files="0" data-text-multiple-selected="files selected" multiple />
 			</label>
 
-			<div class="vf-file-upload-module__input--details vf-file-upload-module__input--details-quota">Up to 20MB</div>
-			<div class="vf-file-upload-module__input--details vf-file-upload-module__input--details-percentage">0%</div>
-			<div class="vf-file-upload-module__input--details vf-file-upload-module__input--details-error">Error</div>
+			<div class="vf-file-upload-module__input--details" data-text-upload-quota="Up to 20MB" data-number-percent-uploaded="0" data-text-error="Error"></div>
 
 			<div class="vf-file-upload-module__button">
 				<button type="submit" class="vf-btn vf-btn--outline-secondary">Submit</button>
@@ -45,3 +29,78 @@ plain: false
 
   
 ```
+
+
+## Custom Upload
+
+If you would prefer to use a different method than the js included here, simply add a `data-custom-upload` attribute to the `vf-file-upload-module` element, and set it as true.
+
+```html
+showSource: true
+plain: false
+---
+
+	<form class="vf-file-upload-module" method="post" action="/" enctype="multipart/form-data" data-custom-upload="true">
+
+		<div class="vf-file-upload-module__icon vf-file-upload-module__icon--doc">
+		</div>
+
+		<div class="vf-file-upload-module__input-container">
+
+			<div class="vf-file-upload-module__input--heading" data-text-uploading="Uploading&hellip;" data-text-drag="Drag & drop your file(s) here" data-text-multiple-uploaded="files uploaded" data-text-single-uploaded="uploaded"></div>
+
+			<label class="vf-file-upload-module__input--label-no-drag" data-text-no-drag-select="Select file(s)">
+				<input type="file" class="vf-file-upload-module__input-file" name="files[]" data-text-number-of-files="0" data-text-multiple-selected="files selected" multiple />
+			</label>
+
+			<div class="vf-file-upload-module__input--details" data-text-upload-quota="Up to 20MB" data-number-percent-uploaded="0" data-text-error="Error"></div>
+
+			<div class="vf-file-upload-module__button">
+				<button type="submit" class="vf-btn vf-btn--outline-secondary">Submit</button>
+			</div>
+			
+		</div>
+
+	</form>
+
+  
+```
+
+
+
+
+
+
+
+Add note about accept: <input type="file"
+  accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
+https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file#attr-accept
+
+
+Have a transition if files upload quickly?
+
+Make js disablable if they prefer a custom solution.
+
+Move IE solution to js, only show if needed.
+
+
+
+
+
+
+vf-file-upload-module__input--label-no-drag only displayed for older browsers.
+
+
+
+
+if custom upload icon is used, it can be filled by animation 
+
+The upload icon is filled by animation, according to the percentage of the upload that has completed. If a custom svg is used, it will also be animated if the relevant part of the svg 
+
+
+Done:
+Different language support
+Move divs to data-components.
+multilingual support
+
+
--- js/components/file-upload-module.js
+++ js/components/file-upload-module.js
@@ -1,141 +1,248 @@
 (function($){
 
 	document.querySelectorAll('.vf-file-upload-module').forEach(function() {
-
-		//Check required browser features are supported
-		var isAdvancedUpload = function() {
-			var div = document.createElement('div');
-			return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
-		}();
 	
-		$('.vf-file-upload-module__icon').each(function( index ) {
+		//Add icon if requested
+		$('.vf-file-upload-module__icon--doc').each(function() {
 			$(this).html('<svg viewBox="0 0 60 80" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><clipPath id="vf-upload-icon-clip-path"><path d="M30,0H0v80h60V26L30,0z M34,14.1l9.2,8H34V14.1z M8,72V8h18v22h26v42H8z"/></clipPath></defs><rect x="0" y="0" fill="rgb(235, 242, 243)" width="100%" height="100%" clip-path="url(#vf-upload-icon-clip-path)" /><rect id="vf-upload-icon--fill" x="0" y="0" fill="rgb(25, 100, 163)" width="100%" height="100%" clip-path="url(#vf-upload-icon-clip-path)" /></svg>');
 
 		});
 
 		var $uploadModule = $('.vf-file-upload-module');
 		var $input = $uploadModule.find('input[type="file"]');
-		$label = $uploadModule.find('.vf-file-upload-module__input-container .vf-file-upload-module__input--heading-files');
-		$quota = $uploadModule.find('.vf-file-upload-module__input-container .vf-file-upload-module__input--details-quota');
 
-		if (isAdvancedUpload) {
+		var $moduleHeading = $uploadModule.find('.vf-file-upload-module__input-container .vf-file-upload-module__input--heading');
+		var $moduleDetails = $uploadModule.find('.vf-file-upload-module__input-container .vf-file-upload-module__input--details');
 
-			$uploadModule.addClass('has-advanced-upload');
+		//Customisable strings
+		var $headingDrag = $moduleHeading.attr("data-text-drag");
+		var $headingUploading = $moduleHeading.attr("data-text-uploading");
+		var $headingSingleUploaded = $moduleHeading.attr("data-text-single-uploaded");
+		var $headingMultipleUploaded = $moduleHeading.attr("data-text-multiple-uploaded");
+		var $detailQuota = $moduleDetails.attr("data-text-upload-quota");
+		//var $detailUploaded = $moduleDetails.attr("data-number-percent-uploaded");
+		var $detailError = $moduleDetails.attr("data-text-error");
 
-			var droppedFiles = false;
+		$moduleHeading.text($headingDrag);
+		$moduleDetails.text($detailQuota);
 
-			$uploadModule.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
-				e.preventDefault();
-				e.stopPropagation();
-			})
-			.on('dragover dragenter', function() {
-				$uploadModule.addClass('is-dragover');
-			})
-			.on('dragleave dragend drop', function() {
-				$uploadModule.removeClass('is-dragover');
-			})
-			.on('drop', function(e) {
-				droppedFiles = e.originalEvent.dataTransfer.files;
+		//$uploadModule.trigger("reset"); //reset the form, just in case anything stored
+
+		if($uploadModule.attr("data-custom-upload") != "true"){
+
+			//Check required browser features are supported for advanced upload
+			var isAdvancedUpload = function() {
+				var div = document.createElement('div');
+				return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+			}();
+
+			if (isAdvancedUpload) {
+
+				$uploadModule.addClass('has-advanced-upload');
+
+				var droppedFiles = false;
+
+				$uploadModule.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+					e.preventDefault();
+					e.stopPropagation();
+				})
+				.on('dragover dragenter', function() {
+					$uploadModule.addClass('is-dragover');
+				})
+				.on('dragleave dragend drop', function() {
+					$uploadModule.removeClass('is-dragover');
+				})
+				.on('drop', function(e) {
+					droppedFiles = e.originalEvent.dataTransfer.files;
+				});
+
+			}
+
+			else{
+
+				$moduleHeading.text("");
+
+			}
+
+			$uploadModule.on('submit', function(e) {
+
+				if ($uploadModule.hasClass('is-uploading')) return false;
+
+				$uploadModule.addClass('is-uploading').removeClass('is-error');
+
+				$moduleHeading.text($headingUploading);
+
+				if (isAdvancedUpload) {
+					
+					e.preventDefault();
+					var ajaxData = new FormData($uploadModule.get(0)); //specify the form
+
+					$moduleDetails.text("0%");
+
+					if (droppedFiles) {
+
+						//ajaxData.append( $input.attr('name'), droppedFiles[0] );
+						$.each( droppedFiles, function(i, file) {
+
+							ajaxData.append( $input.attr('name'), file ); //. $input.attr('name') is just a label
+							console.log(ajaxData.getAll($input.attr('name')));
+
+						});
+
+					}
+
+					$.ajax({
+						url: $uploadModule.attr('action'),
+						type: $uploadModule.attr('method'),
+						data: ajaxData,
+						dataType: 'json',
+						cache: false,
+						contentType: false,
+						processData: false,
+
+						xhr: function() {
+					        var xhr = new window.XMLHttpRequest();
+					        xhr.upload.addEventListener("progress", function(data) {
+
+								if (data.lengthComputable) {
+
+									var percentComplete = (data.loaded / data.total) * 100;
+
+									$moduleDetails.text((Math.round( (percentComplete) * 1 ) / 1) + '%');
+									$moduleDetails.attr("data-number-percent-uploaded", percentComplete);
+
+									var vfUploadIconFill = 100 - percentComplete;
+									$('.vf-file-upload-module__icon--doc svg #vf-upload-icon--fill').css('y', vfUploadIconFill + "%");
+
+
+								} else {
+									console.log('Unable to compute progress information since the total size is unknown');
+								}
+
+					       }, false);
+					       return xhr;
+					    },
+
+						/*progress: function (data) {
+
+							console.log("progress: data");
+
+							console.log(data);
+
+							if (data.lengthComputable) {
+
+								var percentComplete = data.loaded / data.total;
+
+								console.log(data.loaded);
+
+								$moduleDetails.text(percentComplete + '%');
+								$moduleDetails.attr("data-number-percent-uploaded", percentComplete);
+
+								var vfUploadIconFill = 100 - percentComplete;
+								$('.vf-file-upload-module__icon--doc svg #vf-upload-icon--fill').css('y', vfUploadIconFill + "%");
+
+
+							} else {
+								console.log('Unable to compute progress information since the total size is unknown');
+							}
+						},*/
+						complete: function() {
+							$uploadModule.removeClass('is-uploading');
+						},
+						success: function(data) {
+							$uploadModule.addClass( data.success == true ? 'is-success' : 'is-error' );
+							if (!data.success){
+
+								console.log(data.error);
+								console.log(data.files);
+
+								$moduleDetails.html('<span class="vf-file-upload-module__input--details-error">' + $detailError + '</span>');
+
+							}
+							else{
+
+								console.log(data.success);
+								console.log(data.files);
+
+								//success does not always mean that the 100% - if (data.lengthComputable)
+								$moduleHeading.text( $input.attr('data-text-number-of-files') > 1 ? $input.attr('data-text-number-of-files') + " " + $headingMultipleUploaded : $moduleHeading.attr('data-single-filename') + " " + $headingSingleUploaded);
+								//$moduleDetails.html('<span class="vf-file-upload-module__input--details-success">' + $moduleDetails.attr("data-number-percent-uploaded") + '%</span>');
+								$moduleDetails.html('<span class="vf-file-upload-module__input--details-success">100%</span>');
+							}
+						},
+						error: function(data) {
+							$uploadModule.addClass('is-error');
+							$moduleDetails.html('<span class="vf-file-upload-module__input--details-error">' + $detailError + '</span>');
+							console.log("File upload error.");
+							console.log(data);
+
+						}
+					});
+
+				} else {
+
+					//TO DO: HANDLE THESE STATES & Get UPLOADING AMOUNT
+
+					var iframeName = 'uploadiframe' + new Date().getTime();
+					$iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
+
+					$('body').append($iframe);
+					$uploadModule.attr('target', iframeName);
+
+					$iframe.one('load', function() {
+						var data = JSON.parse($iframe.contents().find('body' ).text());
+						$uploadModule
+						.removeClass('is-uploading')
+						.addClass(data.success == true ? 'is-success' : 'is-error')
+						.removeAttr('target');
+						
+						//if (!data.success) $errorMsg.text(data.error);
+
+						$uploadModule.removeAttr('target');
+						$iframe.remove();
+
+					});
+				}
+				
 			});
+
+			var showFiles = function(files) {
+				var $totalSize = 0;
+
+				//Display filename or number of files added
+				$input.attr('data-text-number-of-files', files.length);
+				$moduleHeading.attr('data-single-filename', files.length === 1 ? files[ 0 ].name : "");
+				$moduleHeading.text(files.length > 1 ? (files.length + " " + $input.attr('data-text-multiple-selected') || '') : files[ 0 ].name);
+
+				for (var i  = 0; i < files.length; i++){$totalSize  += files[i].size;}
+
+				//display filesize sum
+				$moduleDetails.text($totalSize < 1000000 ? (Math.round( ($totalSize/1000) * 10 ) / 10) + " KB" : (Math.round( ($totalSize/1000000) * 10 ) / 10) + " MB");
+
+			};
+
+			$uploadModule.on('drop', function(e) {
+
+				$uploadModule.addClass('has-dropped');
+				$('.vf-file-upload-module__icon--doc svg #vf-upload-icon--fill').css('y', "100%"); //Reset
+				droppedFiles = e.originalEvent.dataTransfer.files;
+				showFiles( droppedFiles );
+
+
+				console.log(droppedFiles);
+
+			});
+
+			$input.on('change', function(e) {
+				showFiles(e.target.files);
+			});
+
 
 		}
 
-		$uploadModule.on('submit', function(e) {
-
-			if ($uploadModule.hasClass('is-uploading')) return false;
-
-			$uploadModule.addClass('is-uploading').removeClass('is-error');
-
-			if (isAdvancedUpload) {
-				
-				e.preventDefault();
-				var ajaxData = new FormData($uploadModule.get(1));
-
-				if (droppedFiles) {
-					$.each( droppedFiles, function(i, file) {
-						ajaxData.append( $input.attr('name'), file );
-						console.log($input.attr('name'));
-					});
-				}
-
-				$.ajax({
-					url: $uploadModule.attr('action'),
-					type: $uploadModule.attr('method'),
-					data: ajaxData,
-					dataType: 'json',
-					cache: false,
-					contentType: false,
-					processData: false,
-					progress: function (data) {
-						if (data.lengthComputable) {
-							var percentComplete = data.loaded / data.total;
-							$('.vf-file-upload-module__input--details-percentage').text(percentComplete + '%');
-							var vfUploadIconFill = 100 - percentComplete;
-							$('.vf-file-upload-module__icon svg #vf-upload-icon--fill').css('fill', vfUploadIconFill);
-						} else {
-							console.log('Unable to compute progress information since the total size is unknown');
-						}
-					},
-					complete: function() {
-						$uploadModule.removeClass('is-uploading');
-					},
-					success: function(data) {
-						$uploadModule.addClass( data.success == true ? 'is-success' : 'is-error' );
-						if (!data.success){
-							$errorMsg.text(data.error); //?
-						}
-						else{
-							$label.text($label.text().substring(0, $label.text().length - 8).concat("uploaded"));
-						}
-					},
-					error: function() {
-						$uploadModule.addClass('is-error');
-						console.log("File upload error.");
-					}
-				});
-
-			} else {
-
-				var iframeName = 'uploadiframe' + new Date().getTime();
-				$iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
-
-				$('body').append($iframe);
-				$uploadModule.attr('target', iframeName);
-
-				$iframe.one('load', function() {
-					var data = JSON.parse($iframe.contents().find('body' ).text());
-					$uploadModule
-					.removeClass('is-uploading')
-					.addClass(data.success == true ? 'is-success' : 'is-error')
-					.removeAttr('target');
-					
-					if (!data.success) $errorMsg.text(data.error); //?
-
-					$uploadModule.removeAttr('target');
-					$iframe.remove();
-
-				});
-			}
-			
-		});
-
-		var showFiles = function(files) {
-			var $totalSize = 0;
-			$label.text(files.length > 1 ? ($input.attr('data-multiple-caption') || '').replace( '{count}', files.length ) : files[ 0 ].name);
-			for (var i  = 0; i < files.length; i++){$totalSize  += files[i].size;}
-			$quota.text($totalSize < 1000000 ? (Math.round( ($totalSize/1000) * 10 ) / 10) + " KB" : (Math.round( ($totalSize/1000000) * 10 ) / 10) + " MB");
-
-		};
-
-		$uploadModule.on('drop', function(e) {
-			$uploadModule.addClass('has-dropped');
-			droppedFiles = e.originalEvent.dataTransfer.files; // the files that were dropped
-			showFiles( droppedFiles );
-		});
-
-		$input.on('change', function(e) {
-			showFiles(e.target.files);
-		});
+		else{
+			//Custom upload technique requested.
+		}
 
 	});
 
