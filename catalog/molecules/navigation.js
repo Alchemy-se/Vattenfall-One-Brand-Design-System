20ef3e1b0ad5090e63ea93ef2cc773cf3fbb41e1
Eduard Hallberg <linkit@live.se>
2020-04-08 17:00:03 +0200

    Implemented GA tracking for all pages

--- catalog/molecules/navigation.js
+++ catalog/molecules/navigation.js
@@ -1,66 +1,71 @@
 import { pageLoader } from "@alchemy-se/catalog";
+import ReactGA from "react-ga";
+
+const trackPage = (page) => {
+  ReactGA.pageview(page);
+};
 
 const nav = [
   {
     path: "/css/card",
     title: "- Card",
-    content: pageLoader(() => import("./card.md"))
+    content: pageLoader(() => {trackPage("/css/card"); return import("./card.md")})
   }, {
     path: "/css/button-group",
     title: "- Button Group",
-    content: pageLoader(() => import("./button-group.md"))
+    content: pageLoader(() => {trackPage("/css/button-group"); return import("./button-group.md")})
   }, {
     path: "/css/cookie-banner",
     title: "- Cookie Banner",
-    content: pageLoader(() => import("./cookie-banner.md"))
+    content: pageLoader(() => {trackPage("/css/cookie-banner"); return import("./cookie-banner.md")})
   }, {
     path: "/css/image-block",
     title: "- Image Block",
-    content: pageLoader(() => import("./image-block.md"))
+    content: pageLoader(() => {trackPage("/css/image-block"); return import("./image-block.md")})
   }, {
     path: "/css/image-with-caption",
     title: "- Image with Caption",
-    content: pageLoader(() => import("./image-with-caption.md"))
+    content: pageLoader(() => {trackPage("/css/image-with-caption"); return import("./image-with-caption.md")})
   }, {
     path: "/css/shareable-image",
     title: "- Shareable Image",
-    content: pageLoader(() => import("./shareable-image.md"))
+    content: pageLoader(() => {trackPage("/css/shareable-image"); return import("./shareable-image.md")})
   }, {
     path: "/css/media-block",
     title: "- Media Block",
-    content: pageLoader(() => import("./media-block.md"))
+    content: pageLoader(() => {trackPage("/css/media-block"); return import("./media-block.md")})
   }, {
     path: "/css/linked-list-block",
     title: "- Linked List Block",
-    content: pageLoader(() => import("./linked-list-block.md"))
+    content: pageLoader(() => {trackPage("/css/linked-list-block"); return import("./linked-list-block.md")})
   }, {
     path: "/css/parallax-image-block",
     title: "- Parallax Image Block",
-    content: pageLoader(() => import("./parallax-image-block.md"))
+    content: pageLoader(() => {trackPage("/css/parallax-image-block"); return import("./parallax-image-block.md")})
   }, {
     path: "/css/large-quote",
     title: "- Large Quote",
-    content: pageLoader(() => import("./large-quote.md"))
+    content: pageLoader(() => {trackPage("/css/large-quote"); return import("./large-quote.md")})
   }, {
     path: "/css/small-quote",
     title: "- Small Quote",
-    content: pageLoader(() => import("./small-quote.md"))
+    content: pageLoader(() => {trackPage("/css/small-quote"); return import("./small-quote.md")})
   }, {
     path: "/css/three-column",
     title: "- Three Column",
-    content: pageLoader(() => import("./three-column.md"))
+    content: pageLoader(() => {trackPage("/css/three-column"); return import("./three-column.md")})
   }, {
     path: "/css/topbar",
     title: "- Top bar",
-    content: pageLoader(() => import("./topbar.md"))
+    content: pageLoader(() => {trackPage("/css/topbar"); return import("./topbar.md")})
   }, {
     path: "/css/pagination",
     title: "- Pagination",
-    content: pageLoader(() => import("./pagination.md"))
+    content: pageLoader(() => {trackPage("/css/pagination"); return import("./pagination.md")})
   }, {
     path: "/css/progress",
     title: "- Progress",
-    content: pageLoader(() => import("./progress.md"))
+    content: pageLoader(() => {trackPage("/css/progress"); return import("./progress.md")})
   }
 ]
 export default nav
