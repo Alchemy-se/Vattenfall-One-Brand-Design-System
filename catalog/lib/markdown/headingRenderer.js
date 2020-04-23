59f3c26c6558daf7cdd0715c7ff09bde1d7dae53
Eduard Hallberg <linkit@live.se>
2020-04-23 09:00:04 +0200

    Implemented heading renderer for markdown, adjusted styling of markdown

---
+++ catalog/lib/markdown/headingRenderer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import styles from "./headingRenderer.scss";
+
+const HeadingHashLink = (rawValue) => {
+	const value = rawValue.toLowerCase().replace(/ /g, "-");
+	return (
+		<span className={styles.anchorWrapper}>
+			&nbsp;<a id={value} className={styles.anchor} href={"#"+value}>#</a>
+		</span>
+	);
+};
+
+const Heading = (props) => {
+	console.log("#DEBUG props #", props);
+	switch(props.level) {
+		case 1: {
+			return props.children.map(child =>
+				<h1 key={"heading-"+child.key} className={styles.headingContainer}>
+					{child.props.value}
+					{HeadingHashLink(child.props.value)}
+				</h1>
+			)
+		}
+		case 2: {
+			return props.children.map(child =>
+				<h2 key={"heading-"+child.key} className={styles.headingContainer}>
+					{child.props.value}
+					{HeadingHashLink(child.props.value)}
+				</h2>
+			)
+		}
+		case 3: {
+			return props.children.map(child =>
+				<h3 key={"heading-"+child.key} className={styles.headingContainer}>
+					{child.props.value}
+					{HeadingHashLink(child.props.value)}
+				</h3>
+			)
+		}
+		case 4: {
+			return props.children.map(child =>
+				<h4 key={"heading-"+child.key} className={styles.headingContainer}>
+					{child.props.value}
+					{HeadingHashLink(child.props.value)}
+				</h4>
+			);
+		}
+		default: {
+			return <p>TEST</p>
+		}
+	}
+}
+
+export default Heading;
---
+++ catalog/lib/markdown/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import Heading from "./headingRenderer";
+
+import styles from "./index.scss";
+
+/*
+module.exports = {
+  break: 'br',
+  paragraph: 'p',
+  emphasis: 'em',
+  strong: 'strong',
+  thematicBreak: 'hr',
+  blockquote: 'blockquote',
+  delete: 'del',
+  link: 'a',
+  image: 'img',
+  linkReference: 'a',
+  imageReference: 'img',
+  table: SimpleRenderer.bind(null, 'table'),
+  tableHead: SimpleRenderer.bind(null, 'thead'),
+  tableBody: SimpleRenderer.bind(null, 'tbody'),
+  tableRow: SimpleRenderer.bind(null, 'tr'),
+  tableCell: TableCell,
+  root: Root,
+  text: TextRenderer,
+  list: List,
+  listItem: ListItem,
+  definition: NullRenderer,
+  heading: Heading,
+  inlineCode: InlineCode,
+  code: CodeBlock,
+  html: Html,
+  virtualHtml: VirtualHtml,
+  parsedHtml: ParsedHtml
+}; */
+setTimeout(() => {
+	console.log("#DEBUG 1 #")
+	location.hash = "#autoprefixer";
+}, 3000);
+
+const Markdown = ({source}) => {
+	return (
+		<div className={`${styles.container} ${"markdown-body"}` }>
+			<ReactMarkdown source={source} renderers={{heading: Heading}}/>
+		</div>
+	)
+};
+
+export default Markdown;
