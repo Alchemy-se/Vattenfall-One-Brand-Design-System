4f5f9a226eb539c00fbcc1bb01a31e7e32c63333
Albin Backstrom <albin.backstrom@rhyme.se>
2020-11-23 14:51:17 +0100

    add functional component instead of class

--- catalog/components/colors/colors.js
+++ catalog/components/colors/colors.js
@@ -15,6 +15,8 @@ const markdown = colors + backgroundColor + borders
 const Colors = ({ uri }) => {
 
   const [component, setComponent] = useState('')
+  const [selectedChild, setSelectedChild] = useState('')
+  const [openModal, setOpenModal] = useState(false)
 
   useEffect(() => {
     const fetch = async () => {
@@ -26,10 +28,48 @@ const Colors = ({ uri }) => {
   }, [])
 
 
+  useEffect(() => {
+    if(component){
 
-  useEffect(()=> {
 
-  })
+    Array.from(document.getElementsByClassName("find_by_header")).forEach(function (item) {
+
+      //create new div
+      const div = document.createElement('div');
+      div.setAttribute('id', item.id + "-div");
+
+      //create new span
+      const span = document.createElement('span');
+      span.innerHTML = 'Report issue';
+      span.classList.add("report-issue-button");
+
+      // append the span element inside the <div>
+      div.appendChild(span);
+
+      // get header by id. sets in headingRenderer.js
+      // append new <div><span>Report issue</span></div> element
+      const header = document.getElementById(item.id)
+      header.appendChild(div)
+
+
+      const reportButton = document.getElementById(item.id + "-div")
+      reportButton.addEventListener('click', function (e) {
+
+        const child = component.metadata.children.filter(child => {
+          return child.uri.indexOf("#" + item.id) !== -1;
+
+        })
+        setSelectedChild(child)
+        setOpenModal(true)
+
+
+      })
+    });
+    }
+
+  }, [component])
+
+  console.log('setSelectedChild: ', selectedChild)
   /*
     constructor(props) {
       super(props);
