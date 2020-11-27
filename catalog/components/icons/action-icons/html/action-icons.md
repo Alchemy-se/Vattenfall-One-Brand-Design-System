2a17aee0a1a48cd430ac6c2536c64f2b29b50efe
Pernilla <pernilla.qvist@rhyme.se>
2020-11-27 17:24:11 +0100

    add individual md file to each icon section and add to guidelines

---
+++ catalog/components/icons/action-icons/html/action-icons.md
@@ -0,0 +1,66 @@
+# Action Icons
+````html
+showSource: false
+---
+  <div class="vf-container">
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-activity-log" data-tooltip="vf-icon-activity-log"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-add-contact" data-tooltip="vf-icon-add-contact"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-alert" data-tooltip="vf-icon-alert"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-align-center" data-tooltip="vf-icon-align-center"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-align-left" data-tooltip="vf-icon-align-left"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-app-settings" data-tooltip="vf-icon-app-settings"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-down-left" data-tooltip="vf-icon-arrow-down-left"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-down-right" data-tooltip="vf-icon-arrow-down-right"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-down" data-tooltip="vf-icon-arrow-down"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-left" data-tooltip="vf-icon-arrow-left"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-right" data-tooltip="vf-icon-arrow-right"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-up-left" data-tooltip="vf-icon-arrow-up-left"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-up-right" data-tooltip="vf-icon-arrow-up-right"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-arrow-up" data-tooltip="vf-icon-arrow-up"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-autostart" data-tooltip="vf-icon-autostart"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-blog" data-tooltip="vf-icon-blog"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-call" data-tooltip="vf-icon-call"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-camera" data-tooltip="vf-icon-camera"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-change-availability" data-tooltip="vf-icon-change-availability"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-chat" data-tooltip="vf-icon-chat"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-checkbox" data-tooltip="vf-icon-checkbox"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-download" data-tooltip="vf-icon-download"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-download-pdf" data-tooltip="vf-icon-download-pdf"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-edit" data-tooltip="vf-icon-edit"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-elipsis" data-tooltip="vf-icon-elipsis"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-favourite" data-tooltip="vf-icon-favourite"></span> 
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-filter" data-tooltip="vf-icon-filter"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-gallery" data-tooltip="vf-icon-gallery"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-help" data-tooltip="vf-icon-help"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-in-progress" data-tooltip="vf-icon-in-progress"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-information" data-tooltip="vf-icon-information"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-list-view" data-tooltip="vf-icon-list-view"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-locked" data-tooltip="vf-icon-locked"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-mail" data-tooltip="vf-icon-mail"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-news" data-tooltip="vf-icon-news"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-off" data-tooltip="vf-icon-off"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-open-new-window" data-tooltip="vf-icon-open-new-window"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-pause" data-tooltip="vf-icon-pause"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-phone" data-tooltip="vf-icon-phone"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-pin" data-tooltip="vf-icon-pin"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-play" data-tooltip="vf-icon-play"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-power-outage" data-tooltip="vf-icon-power-outage"></span>
+
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-request" data-tooltip="vf-icon-request"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-restart" data-tooltip="vf-icon-restart"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-send" data-tooltip="vf-icon-send"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-settings" data-tooltip="vf-icon-settings"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-share" data-tooltip="vf-icon-share"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-shop" data-tooltip="vf-icon-shop"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-sign-in" data-tooltip="vf-icon-sign-in"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-sign-out" data-tooltip="vf-icon-sign-out"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-sort" data-tooltip="vf-icon-sort"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-tag" data-tooltip="vf-icon-tag"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-trash" data-tooltip="vf-icon-trash"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-turn-device" data-tooltip="vf-icon-turn-device"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-unlocked" data-tooltip="vf-icon-unlocked"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-user" data-tooltip="vf-icon-user"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-zoom-in" data-tooltip="vf-icon-zoom-in"></span>
+    <span class="p-1 vf-tooltip vf-tooltip--blue-solid vf-icon-zoom-out" data-tooltip="vf-icon-zoom-out"></span>    
+</div>
+````
--- catalog/guidelines/icons/Icons.js
+++ catalog/guidelines/icons/Icons.js
@@ -1,13 +1,29 @@
 import React, { Fragment } from 'react';
 import PageHeader from '../PageHeader';
+import Markdown from "../../lib/markdown";
+import Action from '../../components/icons/action-icons/html/action-icons.md';
+import InCharge from '../../components/icons/incharge-icons/html/incharge-icons.md';
+import Identification from '../../components/icons/identification-icons/html/identification-icons.md';
+import Navigation from '../../components/icons/navigation-icons/html/navigation-icons.md';
+import SmartHome from '../../components/icons/smart-home-icons/html/smart-home-icons.md';
+import SocialMedia from '../../components/icons/socialmedia-icons/html/socialmedia-icons.md';
+import IconBlock from './iconBlock';
+
 
 const Group4 = '/img/guidelines/icons/Group4.png';
-const ActionIcons = '/img/guidelines/icons/ActionIcons.png';
-const InChargeIcons = '/img/guidelines/icons/InChargeIcons.png';
-const IdentificationIcons = '/img/guidelines/icons/IdentificationIcons.png';
-const NavigationIcons = '/img/guidelines/icons/NavigationIcons.png';
-const SmartHomeIcons = '/img/guidelines/icons/SmartHomeIcons.png';
-const SocialMediaIcons = '/img/guidelines/icons/SocialMediaIcons.png';
+// const ActionIcons = '/img/guidelines/icons/ActionIcons.png';
+// const InChargeIcons = '/img/guidelines/icons/InChargeIcons.png';
+// const IdentificationIcons = '/img/guidelines/icons/IdentificationIcons.png';
+// const NavigationIcons = '/img/guidelines/icons/NavigationIcons.png';
+// const SmartHomeIcons = '/img/guidelines/icons/SmartHomeIcons.png';
+// const SocialMediaIcons = '/img/guidelines/icons/SocialMediaIcons.png';
+
+  const editMd = (mdText) => {
+    const hideSourceCode = mdText.replace("showSource: true", "showSource: false");
+    const removeH3 = hideSourceCode.substring(hideSourceCode.indexOf("\n") + 1);
+    return removeH3;
+  }
+
 
 const Icons = () => {
   return (
@@ -15,29 +31,31 @@ const Icons = () => {
       <PageHeader title="The Icons" />
       <section className="p-2 vf-border-top-gray-200"></section>
       <div className="vf-row">
-        <div className="vf-col">
+        <div className="vf-col-12" style={{lineHeight: "44px"}}>
           <p>
             The icons mirror our typeface – engineered and human, confident and determined.
-            <br/>
-            <br/>
-            They are created to be simple, yet powerful, representations to ease and help understanding of our services, products and  ideas. Always used in moderation, and never animated, the icons can together with simple images create more clarity and messaging power than illustrations.
-            <br/>
-            <br/>
-            Icons should always be in black when placed on a white or grey background. The same applies when icons are placed on other light background colours.
-            <br/>
-            <br/>
-            When placed on an image – icons should primarily be white, but depending on the image they are also allowed to be black.
-            <br/>
-            <br/>
-            When icons are in a group, use them in the same size to ensure that they have the same line weight
-            <br/>
-            <br/>
-            – Do not use icons in colour.
-            <br/>
-            – Do not use outlined icons.
-            <br/>
-            – Do not alter or distort the icons.
           </p>
+          <p>
+            They are created to be simple, yet powerful, representations to ease and help understanding of our services, products and  ideas. Always used in moderation, and never animated, the icons can together with simple images create more clarity and messaging power than illustrations.
+            Icons should always be in black when placed on a white or grey background. The same applies when icons are placed on other light background colors.
+          </p>
+          <p>
+            When placed on an image - icons should primarily be white, but depending on the image they are also allowed to be black.
+          </p>
+          <p>
+            When icons are in a group, use them in the same size to ensure that they have the same line weight
+          </p>
+          <ul style={{listStyleType: "none", paddingLeft: "0"}}>
+          <li>
+            - Do not use icons in color.
+          </li>
+          <li>
+            - Do not use outlined icons.
+          </li>
+          <li>
+            - Do not alter or distort the icons.
+          </li>
+          </ul>       
         </div>
       </div>
       <div className="vf-row">
@@ -45,53 +63,42 @@ const Icons = () => {
           <img src={Group4} style={{ width: '100%' }} />
         </div>
       </div>
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">Action Icons</h4>
-        <p>Action icons are used to signal and describe interaction.</p>
-        <div className="vf-col vf-p-0">
-          <img src={ActionIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
-      <div className="vf-mt-md" />
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">InCharge Icons</h4>
-        <p>These are specific Icons for InCharge.</p>
-        <div className="vf-col vf-p-0">
-          <img src={InChargeIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
-      <div className="vf-mt-md" />
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">Identification Icons</h4>
-        <p>Identification icons are used to identify a service or product.</p>
-        <div className="vf-col vf-p-0">
-          <img src={IdentificationIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
-      <div className="vf-mt-md" />
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">Navigation Icons</h4>
-        <p>Navigation icons are used as part of the UI.</p>
-        <div className="vf-col vf-p-0">
-          <img src={NavigationIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
-      <div className="vf-mt-md" />
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">Smart Home Icons</h4>
-        <p>Smart home icons are used to identify products and services for the home.</p>
-        <div className="vf-col vf-p-0">
-          <img src={SmartHomeIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
-      <div className="vf-mt-md" />
-      <div className="row align-items-start">
-        <h4 className="use-in-toc">Social Media Icons</h4>
-        <p>Social media icons do not adhere to our icon style since they are logotypes and symbol of other platforms and services.</p>
-        <div className="vf-col vf-p-0">
-          <img src={SocialMediaIcons} style={{ width: '100%' }} />
-        </div>
-      </div>
+      <IconBlock 
+        title="Action Icons" 
+        text="Action icons are used to signal and describe interaction."
+      >
+        <Markdown source={editMd(Action)} />
+      </IconBlock>
+      <IconBlock 
+        title="InCharge Icons" 
+        text="These are specific Icons for InCharge."
+      >
+        <Markdown source={editMd(InCharge)} />
+      </IconBlock>
+      <IconBlock 
+        title="Identification Icons" 
+        text="Identification icons are used to identify a service or product."
+      >
+        <Markdown source={editMd(Identification)} />
+      </IconBlock>
+      <IconBlock 
+        title="Navigation Icons" 
+        text="Navigation icons are used as part of the UI."
+      >
+        <Markdown source={editMd(Navigation)} />
+      </IconBlock>
+      <IconBlock 
+        title="Smart Home Icons" 
+        text="Smart home icons are used to identify products and services for the home."
+      >
+        <Markdown source={editMd(SmartHome)} />
+      </IconBlock>
+      <IconBlock 
+        title="Social Media Icons" 
+        text="Social media icons do not adhere to our icon style since they are logotypes and symbol of other platforms and services."
+      >
+        <Markdown source={editMd(SocialMedia)} />
+      </IconBlock>     
     </Fragment>
   );
 };
