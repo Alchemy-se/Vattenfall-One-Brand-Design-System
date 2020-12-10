a726115d3e84c6a122a35fa654028674f5e957ec
Albin Backstrom <albin.backstrom@rhyme.se>
2020-12-10 16:58:56 +0100

    .

--- catalog/lib/drawer/getStartedMenu.js
+++ catalog/lib/drawer/getStartedMenu.js
@@ -6,8 +6,12 @@ const MENU_ITEMS = ["/design", "/developer"];
 const MENU_ITEM_NAMES = ["Designers", "Developers"];
 
 const GetStartedMenu = () => {
+
+
 	return MENU_ITEMS.map((item, index) => {
 		return (
+
+
 			<Menu.Item key={"/get-started"+item}>
 				<Link to={"/get-started"+item}>{MENU_ITEM_NAMES[index]}</Link>
 			</Menu.Item>
