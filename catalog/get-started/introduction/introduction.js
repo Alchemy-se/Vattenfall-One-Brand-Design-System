7a8ee8ae77a280f070c33032b0214baa12601e9c
Albin Backstrom <albin.backstrom@rhyme.se>
2020-12-16 08:37:45 +0100

    fix typos

--- catalog/get-started/introduction/introduction.js
+++ catalog/get-started/introduction/introduction.js
@@ -10,7 +10,7 @@ const Introduction = () => {
 
        <span> No matter if you are a designer or a developer, we have created a tool / modular system with the Digital Design
         System for you to map and manage all processes in the creation of an online project according to our brand and
-        technical standards. By synchronizing the modules and components in Abstract, Sketch and Githup we enable
+        technical standards. By synchronizing the modules and components in Abstract, Sketch and GitHub we enable
         designers and developers to use the same basis to create a stringent and uniform result according to the
         Vattenfall Brand Expression.</span>
 
