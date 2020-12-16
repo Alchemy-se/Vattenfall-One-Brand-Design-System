47efde964542bef4c64e666e68230bdc91ebca00
Albin Backstrom <albin.backstrom@rhyme.se>
2020-12-16 16:53:37 +0100

    feat(carousel): new carousel and hero modules

--- metadata/entry.js
+++ metadata/entry.js
@@ -1,11 +1,15 @@
-require('dotenv').config({path:__dirname+'/../.env.production'})
-//require('dotenv').config({path:__dirname+`/../.env.${process.env.NODE_ENV}`})
+//require('dotenv').config({path:__dirname+'/../.env.production'})
+require('dotenv').config({path:__dirname+`/../.env.${process.env.NODE_ENV}`})
 
 const { buildComponentMetadata } = require("./build-metadata");
 const { uploadAlgoliaData } = require("./upload-algolia-metadata");
 const { uploadMetadata } = require("./upload-metadata-to-strapi");
 const { buildAlgoliadMetadataJSON } = require("./build-agolia-metadata");
+
+
 Promise.all([buildComponentMetadata()]).then(() => {
+
+
   Promise.all([buildAlgoliadMetadataJSON()])
     .then(() => {
       Promise.all([uploadAlgoliaData(), uploadMetadata()])
