936c367d84479864342f5e54cc4a420e71e9585c
Albin Backstrom <albin.backstrom@rhyme.se>
2021-01-11 08:48:07 +0100

    add new subject and body to mail when signing up for newsletter

--- catalog/lib/newsletterBox/index.js
+++ catalog/lib/newsletterBox/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./index.scss";
 const NewsletterBox = () => {
+  const subject = "Sign up for the DDS Newsletter";
+  const body = "Please sign me up with these contact details for the Digital Design System Newsletter.";
   return (
-    <div onClick={() => location.href = 'mailto:branding@vattenfall.com'
+    <div onClick={() => location.href = `mailto:branding@vattenfall.com?subject=${subject}&body=${body}`
 
     } className={styles.container}>
 
