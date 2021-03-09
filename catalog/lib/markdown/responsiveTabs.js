9e09dec6c07b47181ff3bd3d7d361d8c48285634
jonatan <jonatan.malm@rhyme.se>
2021-03-09 16:32:55 +0100

    responsive tabs

--- catalog/lib/markdown/responsiveTabs.js
+++ catalog/lib/markdown/responsiveTabs.js
@@ -1,29 +1,24 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import styles from "./responsiveTabs.scss";
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import styles from './responsiveTabs.scss'
+import ResponsiveTabs2 from './ResponsiveTabs2'
 
 const responsiveSizes = [
   { name: 'Phone', width: 375, height: 667 },
   { name: 'Tablet', width: 768, height: 1024 },
   { name: 'Desktop', width: 1920, height: 1080 },
-];
+]
 
 class Frame extends Component {
   componentDidMount() {
     this.renderFrameContent();
   }
-
-
   renderFrameContent() {
     // let doc = document.querySelector('.responsiveTabs__iframe___1nTCV')
     //   .contentDocument
-    
     let responsiveTabs = document.querySelectorAll('.responsiveTabs__iframe___1nTCV')
- 
     responsiveTabs.forEach(tab => {
-
          let doc = tab.contentDocument
-
       if (doc.readyState === 'interactive' || doc.readyState === "complete") {
         let contents = React.createElement(
           'div',
@@ -54,11 +49,9 @@ class Frame extends Component {
       }
     })
   }
-
-  componentDidUpdate() {
-    this.renderFrameContent();
-  }
-
+  // componentDidUpdate() {
+  //   this.renderFrameContent();
+  // }
   render() {
     return <iframe id={"iframe"} className={styles.iframe} />;
   }
@@ -66,19 +59,19 @@ class Frame extends Component {
 
 export default class ResponsiveTabs extends Component {
   constructor(props) {
-    super(props);
-    this.iframe = React.createRef();
-    this.iframeWrapper = React.createRef();
-    this.containerRef = React.createRef();
+    super(props)
+    this.iframe = React.createRef()
+    this.iframeWrapper = React.createRef()
+    this.containerRef = React.createRef()
     this.state = {
       selectedTab: responsiveSizes[0].name,
       selectedWidth: responsiveSizes[0].width,
-      iframeRef: React.createRef()
-    };
+      iframeRef: React.createRef(),
+    }
   }
 
   componentDidMount() {
-    window.addEventListener('resize', this.resize);
+    window.addEventListener('resize', this.resize)
   }
 
   componentWillUnmount() {
@@ -86,104 +79,137 @@ export default class ResponsiveTabs extends Component {
   }
 
   resize = () => {
-    this.setState({});
+    this.setState({})
   }
 
-  renderSizeIcon = (name) => {
+  renderSizeIcon = name => {
     switch (name) {
-      case "Phone": {
+      case 'Phone': {
         return (
-          <svg viewBox="0 0 2 2">
-            <rect className="catalog-11861ax-Preview-className" width="0.5622188905547226" height="1"
-                  x="0.7188905547226387" y="0.5"></rect>
+          <svg viewBox='0 0 2 2'>
+            <rect
+              className='catalog-11861ax-Preview-className'
+              width='0.5622188905547226'
+              height='1'
+              x='0.7188905547226387'
+              y='0.5'
+            ></rect>
           </svg>
-        );
+        )
       }
-      case "Tablet": {
+      case 'Tablet': {
         return (
-          <svg viewBox="0 0 2 2">
-            <rect className="catalog-11861ax-Preview-className" width="0.75" height="1" x="0.625" y="0.5"></rect>
+          <svg viewBox='0 0 2 2'>
+            <rect
+              className='catalog-11861ax-Preview-className'
+              width='0.75'
+              height='1'
+              x='0.625'
+              y='0.5'
+            ></rect>
           </svg>
-        );
+        )
       }
-      case "Desktop": {
+      case 'Desktop': {
         return (
-          <svg viewBox="0 0 2 2">
-            <rect className="catalog-11861ax-Preview-className" width="1.7777777777777777" height="1"
-                  x="0.11111111111111116" y="0.5"></rect>
+          <svg viewBox='0 0 2 2'>
+            <rect
+              className='catalog-11861ax-Preview-className'
+              width='1.7777777777777777'
+              height='1'
+              x='0.11111111111111116'
+              y='0.5'
+            ></rect>
           </svg>
-        );
+        )
       }
       default: {
-
       }
     }
-  };
+  }
 
   selectTab = (name, width) => {
-    if (this.state.selectedTab === name) return;
+    if (this.state.selectedTab === name) return
     this.setState({
       selectedTab: name,
-      selectedWidth: width
-    });
-  };
+      selectedWidth: width,
+    })
+  }
 
   renderTabs = () => {
     return responsiveSizes.map((item, index) => {
-      const tabClasses = item.name === this.state.selectedTab ? styles.tabSelected : styles.tab;
+      const tabClasses =
+        item.name === this.state.selectedTab ? styles.tabSelected : styles.tab
       return (
-        <div className={tabClasses} onClick={() => this.selectTab(item.name, item.width)} key={index}>
+        <div
+          className={tabClasses}
+          onClick={() => this.selectTab(item.name, item.width)}
+          key={index}
+        >
           <div className={styles.iconContainer}>
             {this.renderSizeIcon(item.name)}
           </div>
           <div className={styles.titleContainer}>
             {item.name}
             <div className={styles.titleInnerContainer}>
-              {item.width + "x" + item.height}
+              {item.width + 'x' + item.height}
             </div>
           </div>
         </div>
       )
-    });
-  };
+    })
+  }
 
   getFrameContainerClassName() {
-    if (this.state.selectedTab === "Phone") {
-      return styles.iframePhone;
-    } else if (this.state.selectedTab === "Tablet") {
-      return styles.iframeTablet;
+    if (this.state.selectedTab === 'Phone') {
+      return styles.iframePhone
+    } else if (this.state.selectedTab === 'Tablet') {
+      return styles.iframeTablet
     } else {
-      return styles.iframeDesktop;
+      return styles.iframeDesktop
     }
   }
 
   getStyle() {
     if (this.iframeWrapper && this.iframeWrapper.current) {
-      const scale = Math.min(1, this.iframeWrapper.current.offsetWidth / this.state.selectedWidth);
-      return { transform: "scale(" + scale + ")" };
+      const scale = Math.min(
+        1,
+        this.iframeWrapper.current.offsetWidth / this.state.selectedWidth
+      )
+      return { transform: 'scale(' + scale + ')' }
     }
-    return {};
+    return {}
   }
 
   getContainerStyle = () => {
     if (this.iframeWrapper && this.iframeWrapper.current) {
-      const scale = Math.min(1, this.iframeWrapper.current.offsetWidth / this.state.selectedWidth);
-      return { width: "100%", height: "100%", maxHeight: "calc(1080px * " + scale + ")" };
+      const scale = Math.min(
+        1,
+        this.iframeWrapper.current.offsetWidth / this.state.selectedWidth
+      )
+      return {
+        width: '100%',
+        height: '100%',
+        maxHeight: 'calc(1080px * ' + scale + ')',
+      }
     }
-    return { width: "100%", height: "100%", maxHeight: "1080px" };
-  };
+    return { width: '100%', height: '100%', maxHeight: '1080px' }
+  }
 
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.menuContainer}>
-          {this.renderTabs()}
-        </div>
+        <div className={styles.menuContainer}>{this.renderTabs()}</div>
         <div className={styles.html} ref={this.containerRef}>
           <div style={this.getContainerStyle()} ref={this.iframeWrapper}>
-            <div className={this.getFrameContainerClassName()} style={this.getStyle()}>
+            <div
+              className={this.getFrameContainerClassName()}
+              style={this.getStyle()}
+            >
               <Frame>
-                <div dangerouslySetInnerHTML={{ __html: this.props.children }} />
+                <div
+                  dangerouslySetInnerHTML={{ __html: this.props.children }}
+                />
               </Frame>
             </div>
           </div>
