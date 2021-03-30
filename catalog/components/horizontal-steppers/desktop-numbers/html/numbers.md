4f93beac7b714cc2c9b4765d6819aa2c9896f54b
mistermalm <jonatan.malm@live.se>
2021-03-30 14:28:12 +0200

    numbers

--- catalog/components/horizontal-steppers/desktop-numbers/html/numbers.md
+++ catalog/components/horizontal-steppers/desktop-numbers/html/numbers.md
@@ -6,29 +6,29 @@
 <div class="vf-horizontal-steppers--container">
 
     <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-1" class="vf-horizontal-steppers--bullet vf-horizontal-steppers--complete">
-            <span class="vf-horizontal-steppers--number">1</span>
+        <div id="vf-horizontal-step-1" class="vf-horizontal-steppers--bullet-numbers">
+            <span class="vf-horizontal-steppers--numbers">1</span>
         </div>
         <label for="vf-horizontal-step-1">Lorem ipsum</label>
     </div>
 
     <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-2" class="vf-horizontal-steppers--bullet vf-horizontal-steppers--active">
-            <span class="vf-horizontal-steppers--number">2</span>
+        <div id="vf-horizontal-step-2" class="vf-horizontal-steppers--bullet-numbers vf-horizontal-steppers--active-numbers">
+            <span class="vf-horizontal-steppers--numbers">2</span>
         </div>
         <label for="vf-horizontal-step-2">Lorem ipsum</label>
     </div>
 
     <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-3" class="vf-horizontal-steppers--bullet">
-            <span class="vf-horizontal-steppers--number">3</span>
+        <div id="vf-horizontal-step-3" class="vf-horizontal-steppers--bullet-numbers">
+            <span class="vf-horizontal-steppers--numbers">3</span>
         </div>
         <label for="vf-horizontal-step-3">Lorem ipsum</label>
     </div>
     
     <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-4" class="vf-horizontal-steppers--bullet">
-            <span class="vf-horizontal-steppers--number">4</span>
+        <div id="vf-horizontal-step-4" class="vf-horizontal-steppers--bullet-numbers">
+            <span class="vf-horizontal-steppers--numbers">4</span>
         </div>
         <label for="vf-horizontal-step-4">Lorem ipsum</label>
     </div>
--- js/components/horizontal-steppers.js
+++ js/components/horizontal-steppers.js
@@ -1,33 +1,68 @@
 ;(function ($) {
-  const bullets = document.getElementsByClassName(
+  // DEFAULT
+  const defaultuBullets = document.getElementsByClassName(
     'vf-horizontal-steppers--bullet'
   )
 
-  function addActiveClass(bullet) {
+  function addActiveClassDefault(bullet) {
     bullet.children[0].classList.add('vf-icon-edit')
     bullet.children[0].classList.add('vf-horizontal-steppers--vf-icon-edit')
+    bullet.nextElementSibling.style = 'font-weight:bold;'
   }
 
-  function addCompleteClass(bullet) {
+  function addCompleteClassDefault(bullet) {
     bullet.children[0].classList.add('vf-icon-check')
   }
 
-  for (let i = 0; i < bullets.length; i++) {
-    let isActive = bullets[i].classList.contains(
-      'vf-horizontal-steppers--active'
-    )
-
-    let isComplete = bullets[i].classList.contains(
-      'vf-horizontal-steppers--complete'
-    )
-
-    if (isActive) {
-      addActiveClass(bullets[i])
-      bullets[i].nextElementSibling.style = 'font-weight:bold;'
-    }
-
-    if (isComplete) {
-      addCompleteClass(bullets[i])
-    }
+  for (let i = 0; i < defaultuBullets.length; i++) {
+    // let isActiveDefault = defaultuBullets[i].classList.contains(
+    //   'vf-horizontal-steppers--active-default'
+    // )
+    // let isCompleteDefault = defaultuBullets[i].classList.contains(
+    //   'vf-horizontal-steppers--complete-default'
+    // )
+    // if (isActiveDefault) {
+    //   addActiveClassDefault(bullets[i])
+    // }
+    // if (isCompleteDefault) {
+    //   addCompleteClassDefault(bullets[i])
+    // }
   }
+
+  // NUMBERS
+  function numbersTabBar() {
+    const numbersBullets = document.getElementsByClassName(
+      'vf-horizontal-steppers--bullet-numbers'
+    )
+    function activeIndex() {
+      numbersBulletsArray = []
+      for (let i = 0; i < numbersBullets.length; i++) {
+        numbersBulletsArray.push(numbersBullets[i])
+      }
+
+      let filterActive = numbersBulletsArray.filter(bullet =>
+        bullet.classList.contains('vf-horizontal-steppers--active-numbers')
+      )
+
+      let activeIndex = numbersBulletsArray.indexOf(filterActive[0])
+      return activeIndex
+    }
+    function addActiveClassNumbers() {
+      numbersBullets[activeIndex()].nextElementSibling.style =
+        'font-weight:bold;'
+    }
+    function completePreviousNumbers() {
+      for (let i = 0; i < activeIndex(); i++) {
+        numbersBullets[i].classList.add(
+          'vf-horizontal-steppers--complete-numbers'
+        )
+        numbersBullets[i].children[0].style = 'color:#fff;'
+      }
+    }
+    function numbersOnChange() {}
+    addActiveClassNumbers()
+    completePreviousNumbers()
+    numbersOnChange()
+  }
+  numbersTabBar()
 })(jQuery)
