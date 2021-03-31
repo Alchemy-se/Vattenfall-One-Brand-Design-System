5222c25e47df95d2b65011125978bc3779acd772
mistermalm <jonatan.malm@live.se>
2021-03-31 15:07:57 +0200

    default + numbers

--- catalog/components/horizontal-steppers/desktop-default/html/default.md
+++ catalog/components/horizontal-steppers/desktop-default/html/default.md
@@ -4,33 +4,44 @@
 <!-- showSource: true
 --- -->
 <div class="vf-horizontal-steppers--container">
+  <div class="vf-horizontal-steppers--step">
+    <div
+      id="vf-horizontal-step-1"
+      class="vf-horizontal-steppers--bullet-default"
+    >
+      <span class="vf-horizontal-steppers--icon"></span>
+    </div>
+    <label for="vf-horizontal-step-1">Lorem ipsum</label>
+  </div>
 
-    <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-1" class="vf-horizontal-steppers--bullet-default vf-horizontal-steppers--complete-default">
-            <span class="vf-horizontal-steppers--icon"></span>
-        </div>
-        <label for="vf-horizontal-step-1">Lorem ipsum</label>
+  <div class="vf-horizontal-steppers--step">
+    <div
+      id="vf-horizontal-step-2"
+      class="vf-horizontal-steppers--bullet-default "
+    >
+      <span class="vf-horizontal-steppers--icon"></span>
     </div>
+    <label for="vf-horizontal-step-2">Lorem ipsum</label>
+  </div>
 
-    <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-2" class="vf-horizontal-steppers--bullet-default vf-horizontal-steppers--active-default">
-            <span class="vf-horizontal-steppers--icon"></span>
-        </div>
-        <label for="vf-horizontal-step-2">Lorem ipsum</label>
+  <div class="vf-horizontal-steppers--step">
+    <div
+      id="vf-horizontal-step-3"
+      class="vf-horizontal-steppers--bullet-default vf-horizontal-steppers--active-default"
+    >
+      <span class="vf-horizontal-steppers--icon"></span>
     </div>
+    <label for="vf-horizontal-step-3">Lorem ipsum</label>
+  </div>
 
-    <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-3" class="vf-horizontal-steppers--bullet-default">
-            <span class="vf-horizontal-steppers--icon"></span>
-        </div>
-        <label for="vf-horizontal-step-3">Lorem ipsum</label>
+  <div class="vf-horizontal-steppers--step">
+    <div
+      id="vf-horizontal-step-4"
+      class="vf-horizontal-steppers--bullet-default"
+    >
+      <span class="vf-horizontal-steppers--icon"></span>
     </div>
-    
-    <div class="vf-horizontal-steppers--step">
-        <div id="vf-horizontal-step-4" class="vf-horizontal-steppers--bullet-default">
-            <span class="vf-horizontal-steppers--icon"></span>
-        </div>
-        <label for="vf-horizontal-step-4">Lorem ipsum</label>
-    </div>
+    <label for="vf-horizontal-step-4">Lorem ipsum</label>
+  </div>
 </div>
 ```
--- js/components/horizontal-steppers.js
+++ js/components/horizontal-steppers.js
@@ -1,68 +1,79 @@
-;(function ($) {
+(function ($) {
   // DEFAULT
-  const defaultuBullets = document.getElementsByClassName(
-    'vf-horizontal-steppers--bullet'
-  )
 
-  function addActiveClassDefault(bullet) {
-    bullet.children[0].classList.add('vf-icon-edit')
-    bullet.children[0].classList.add('vf-horizontal-steppers--vf-icon-edit')
-    bullet.nextElementSibling.style = 'font-weight:bold;'
-  }
+  // function addActiveClassDefault(bullet) {
+  //   bullet.children[0].classList.add('vf-icon-edit');
+  //   bullet.children[0].classList.add('vf-horizontal-steppers--vf-icon-edit');
+  //   bullet.nextElementSibling.style = 'font-weight:bold;';
+  // }
 
-  function addCompleteClassDefault(bullet) {
-    bullet.children[0].classList.add('vf-icon-check')
-  }
+  // function addCompleteClassDefault(bullet) {
+  //   bullet.children[0].classList.add('vf-icon-check');
+  // }
 
-  for (let i = 0; i < defaultuBullets.length; i++) {
-    // let isActiveDefault = defaultuBullets[i].classList.contains(
-    //   'vf-horizontal-steppers--active-default'
-    // )
-    // let isCompleteDefault = defaultuBullets[i].classList.contains(
-    //   'vf-horizontal-steppers--complete-default'
-    // )
-    // if (isActiveDefault) {
-    //   addActiveClassDefault(bullets[i])
-    // }
-    // if (isCompleteDefault) {
-    //   addCompleteClassDefault(bullets[i])
-    // }
+  // for (let i = 0; i < defaultuBullets.length; i++) {
+  //   // let isActiveDefault = defaultuBullets[i].classList.contains(
+  //   //   'vf-horizontal-steppers--active-default'
+  //   // )
+  //   // let isCompleteDefault = defaultuBullets[i].classList.contains(
+  //   //   'vf-horizontal-steppers--complete-default'
+  //   // )
+  //   // if (isActiveDefault) {
+  //   //   addActiveClassDefault(bullets[i])
+  //   // }
+  //   // if (isCompleteDefault) {
+  //   //   addCompleteClassDefault(bullets[i])
+  //   // }
+  // }
+
+  function activeIndex(bullets, bulletType) {
+    bulletsArray = [];
+    for (let i = 0; i < bullets.length; i++) {
+      bulletsArray.push(bullets[i]);
+    }
+
+    let filterActive = bulletsArray.filter(bullet =>
+      bullet.classList.contains(`vf-horizontal-steppers--active-${bulletType}`)
+    );
+
+    let activeIndex = bulletsArray.indexOf(filterActive[0]);
+    return activeIndex;
   }
 
   // NUMBERS
-  function numbersTabBar() {
-    const numbersBullets = document.getElementsByClassName(
-      'vf-horizontal-steppers--bullet-numbers'
-    )
-    function activeIndex() {
-      numbersBulletsArray = []
-      for (let i = 0; i < numbersBullets.length; i++) {
-        numbersBulletsArray.push(numbersBullets[i])
+  function horizontalTabBar(type) {
+    const bullets = document.getElementsByClassName(
+      `vf-horizontal-steppers--bullet-${type}`
+    );
+
+    function addActiveClass(type) {
+      if (type === 'default') {
+        bullets[activeIndex(bullets, type)].children[0].classList.add(
+          'vf-icon-edit'
+        );
+        bullets[activeIndex(bullets, type)].children[0].classList.add(
+          "'vf-horizontal-steppers--vf-icon-edit'"
+        );
       }
-
-      let filterActive = numbersBulletsArray.filter(bullet =>
-        bullet.classList.contains('vf-horizontal-steppers--active-numbers')
-      )
-
-      let activeIndex = numbersBulletsArray.indexOf(filterActive[0])
-      return activeIndex
+      bullets[activeIndex(bullets, type)].nextElementSibling.style =
+        'font-weight:bold;';
     }
-    function addActiveClassNumbers() {
-      numbersBullets[activeIndex()].nextElementSibling.style =
-        'font-weight:bold;'
-    }
-    function completePreviousNumbers() {
-      for (let i = 0; i < activeIndex(); i++) {
-        numbersBullets[i].classList.add(
-          'vf-horizontal-steppers--complete-numbers'
-        )
-        numbersBullets[i].children[0].style = 'color:#fff;'
+
+    function completePrevious(type) {
+      for (let i = 0; i < activeIndex(bullets, type); i++) {
+        if (type === 'default') {
+          bullets[i].children[0].classList.add('vf-icon-check');
+        }
+        bullets[i].classList.add(`vf-horizontal-steppers--complete-${type}`);
+        bullets[i].children[0].style = 'color:#fff;';
       }
     }
-    function numbersOnChange() {}
-    addActiveClassNumbers()
-    completePreviousNumbers()
-    numbersOnChange()
+
+    if (bullets.length > 0) {
+      addActiveClass(type);
+      completePrevious(type);
+    }
   }
-  numbersTabBar()
-})(jQuery)
+  horizontalTabBar('default');
+  horizontalTabBar('numbers');
+})(jQuery);
